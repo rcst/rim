0c4faaf55f46f512aa82a44cce8f04c788e20b87
Eric Stemmler <rcst@posteo.de>
2021-05-24 15:20:38 +0100

    fixed failing engine test for newly build package, simplified testing call

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -1,12 +1,12 @@
 test_that("maxima knitr engine works", {
-	    fr <- system.file("extdata", 
-			      c("test.Rmd", "result.md"), 
+	    fr <- system.file("extdata", c("test.Rmd", "result.md"), 
 			      package = "rmaxima")
-	    fo <- paste0(dirname(fr[1]), "/test.knit.md")
+	    fo <- paste0(dirname(fr[1]), "/test.md")
 
 	    hash <- digest(readLines(fr[2]), "sha256")
 
-	    rmarkdown::render(fr[1], quiet = TRUE, run_pandoc = FALSE)
+	    # rmarkdown::render(fr[1], quiet = TRUE, run_pandoc = FALSE)
+	    knit(input = fr[1], output = fo, quiet = TRUE)
 
 	    expect_match(digest(readLines(fo), "sha256"), hash)
 
