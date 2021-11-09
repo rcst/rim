d83b03677adafd8c23162701e4f1e52623cb3ea5
Eric Stemmler <rcst@posteo.de>
2021-11-09 23:25:03 +0100

    draw functions now return resulting file name, still some things to fix

--- inst/extdata/plot-knitr-pdf.lisp
+++ inst/extdata/plot-knitr-pdf.lisp
@@ -7,88 +7,53 @@
 (defvar *builtin-draw2d* (symbol-function '$draw2d))
 (defvar *builtin-draw3d* (symbol-function '$draw3d))
 
-;; ($set_random_state ($make_random_state ($absolute_real_time)))
-
-;; if a random file name is used, every time the knitr engine is run
-;; the same plot files with different names are saved
-;; idea: generate hash from plot arguments string and use as filename
-;;       however, this will overwrite the same file, but will work
-;;       to avoid exhausting path lengths, only use first 6 digits of sha256sum
-;;       maybe save plots in folder _maxima_files
 (defmfun $plot2d (&rest args)
   (if (member '$pdf_file args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-plot2d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "/plot2d" ($simplode args))) 1 7))
        (pdf-file ($sconcat $plot_output_folder "/plot2d-" nnn ".pdf"))
        (args-new (append args (list `((mlist) $pdf_file ,pdf-file)))))
-      ;; (format t "plot2d: append pdf file ~s to arguments and call built-in plot2d.~%" pdf-file)
-      ;; (format t "plot2d: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
       (apply *builtin-plot2d* args-new))))
 
 (defmfun $plot3d (&rest args)
   (if (member '$pdf_file args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-plot3d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "plot3d" ($simplode args))) 1 7))
        (pdf-file ($sconcat $plot_output_folder "/plot3d-" nnn ".pdf"))
        (args-new (append args (list `((mlist) $pdf_file ,pdf-file)))))
-      ;; (format t "plot3d: append pdf file ~s to arguments and call built-in plot2d.~%" pdf-file)
-      ;; (format t "plot3d: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
       (apply *builtin-plot3d* args-new))))
 
 (defmfun $draw (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $pdf)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw* args-new))))
+      (apply *builtin-draw* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".pdf")))))
 
 (defmfun $draw2d (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw2d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw2d" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw2d-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $pdf)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw2d* args-new))))
+      (apply *builtin-draw2d* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".pdf")))))
 
 (defmfun $draw3d (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw3d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw3d" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw3d-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $pdf)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw3d* args-new))))
+      (apply *builtin-draw3d* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".pdf")))))
--- inst/extdata/plot-knitr-png.lisp
+++ inst/extdata/plot-knitr-png.lisp
@@ -7,88 +7,53 @@
 (defvar *builtin-draw2d* (symbol-function '$draw2d))
 (defvar *builtin-draw3d* (symbol-function '$draw3d))
 
-;; ($set_random_state ($make_random_state ($absolute_real_time)))
-
-;; if a random file name is used, every time the knitr engine is run
-;; the same plot files with different names are saved
-;; idea: generate hash from plot arguments string and use as filename
-;;       however, this will overwrite the same file, but will work
-;;       to avoid exhausting path lengths, only use first 6 digits of sha256sum
-;;       maybe save plots in folder _maxima_files
 (defmfun $plot2d (&rest args)
   (if (member '$png_file args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-plot2d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "plot2d" ($simplode args))) 1 7))
-       (png-file ($sconcat $plot2d_output_folder "/plot2d-" nnn ".png"))
+       (png-file ($sconcat $plot_output_folder "/plot2d-" nnn ".png"))
        (args-new (append args (list `((mlist) $png_file ,png-file)))))
-      ;; (format t "plot2d: append png file ~s to arguments and call built-in plot2d.~%" png-file)
-      ;;(format t "plot2d: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
       (apply *builtin-plot2d* args-new))))
 
 (defmfun $plot3d (&rest args)
   (if (member '$png_file args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-plot3d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "plot3d" ($simplode args))) 1 7))
-       (png-file ($sconcat $plot3d_output_folder "/plot3d-" nnn ".png"))
+       (png-file ($sconcat $plot_output_folder "/plot3d-" nnn ".png"))
        (args-new (append args (list `((mlist) $png_file ,png-file)))))
-      ;; (format t "plot3d: append png file ~s to arguments and call built-in plot2d.~%" png-file)
-      ;; (format t "plot3d: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
       (apply *builtin-plot3d* args-new))))
 
 (defmfun $draw (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $png)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw* args-new))))
+      (apply *builtin-draw* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".png")))))
 
 (defmfun $draw2d (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw2d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw2d" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw2d-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $png)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw2d* args-new))))
+      (apply *builtin-draw2d* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".png")))))
 
 (defmfun $draw3d (&rest args)
   (if (member '$file_name args)
-    ;; Hmm. plot2d args already contains svg_file.
-    ;; Just punt the call without modifying the arguments.
     (apply *builtin-draw3d* args)
-    ;; Otherwise, append another argument for svg_file,
-    ;; and then punt to built-in plot2d.
     (let*
       ((nnn ($substring ($sha256sum ($sconcat "draw3d" ($simplode args))) 1 7))
        (file_name ($sconcat $plot_output_folder "/draw3d-" nnn))
        (args-new (append args (list `((mequal) $file_name ,file_name))))
        (args-new (append args-new (list `((mequal) $terminal $png)))))
-       ;; (format t "draw: append pdf file ~s to arguments and call built-in draw.~%" file_name)
-       ;; (format t "draw: new args: ~a~%" (mfuncall '$string (cons '(mlist) args-new)))
-      (apply *builtin-draw3d* args-new))))
+      (apply *builtin-draw3d* args-new)
+      `((mlist) ,file_name ,($sconcat file_name ".png")))))
