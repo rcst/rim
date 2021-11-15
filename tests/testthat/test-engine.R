d2b4e46eb2a9bce46dae3737b3ac460f99950cc4
Eric Stemmler <rcst@posteo.de>
2021-11-16 00:53:11 +0100

    changed knitr engine test to html based

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -4,16 +4,16 @@ test_that("maxima knitr engine works", {
   if(.Platform$OS.type == "windows")
     skip("Engine test takes too long under windows")
 
-  fr <- system.file("extdata", c("test.Rmd", "result.md"),
+  fr <- system.file("extdata", c("test.Rmd", "result.html"),
 		    package = "rim", mustWork = TRUE)
-  fo <- paste0(dirname(fr[1]), "/test.md")
+  fo <- paste0(dirname(fr[1]), "/test.html")
 
   hash <- digest::digest(readLines(con = fr[2]), "sha256")
-  suppressWarnings(knit(input = fr[1], output = fo, quiet = TRUE, envir = .GlobalEnv))
-#   suppressWarnings(rmarkdown::render(input=fr[1], 
-# 		    output_dir = dirname(fr[1]), 
-# 		    output_file = "test.html", 
-# 		    quiet = TRUE))
+  # suppressWarnings(knit(input = fr[1], output = fo, quiet = TRUE, envir = .GlobalEnv))
+  suppressWarnings(rmarkdown::render(input=fr[1], 
+		    output_dir = dirname(fr[1]), 
+		    output_file = "test.html", 
+		    quiet = TRUE))
 
   expect_match(digest::digest(readLines(fo), "sha256"), hash)
 
