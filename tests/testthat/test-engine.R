f8a018cf816cc775cc64e3551c5497d4a2318b4c
Eric Stemmler <rcst@posteo.de>
2021-11-16 01:05:52 +0100

    just another test fix

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -4,16 +4,16 @@ test_that("maxima knitr engine works", {
   if(.Platform$OS.type == "windows")
     skip("Engine test takes too long under windows")
 
-  fr <- system.file("extdata", c("test.Rmd", "result.html"),
+  fr <- system.file("extdata", c("test.Rmd", "result.md"),
 		    package = "rim", mustWork = TRUE)
-  fo <- paste0(dirname(fr[1]), "/test.html")
+  fo <- paste0(dirname(fr[1]), "/test.md")
 
   hash <- digest::digest(readLines(con = fr[2]), "sha256")
-  # suppressWarnings(knit(input = fr[1], output = fo, quiet = TRUE, envir = .GlobalEnv))
-  suppressWarnings(rmarkdown::render(input=fr[1], 
-		    output_dir = dirname(fr[1]), 
-		    output_file = "test.html", 
-		    quiet = TRUE))
+  suppressWarnings(knit(input = fr[1], output = fo, quiet = TRUE, envir = .GlobalEnv))
+#  suppressWarnings(rmarkdown::render(input=fr[1], 
+#		    output_dir = dirname(fr[1]), 
+#		    output_file = "test.html", 
+#		    quiet = TRUE))
 
   expect_match(digest::digest(readLines(fo), "sha256"), hash)
 
