f0038179855139087f962110d5db03761512219d
rcst <rcst@posteo.de>
2025-11-25 09:07:52 +0100

    minor

--- index.R
+++ index.R
@@ -2,7 +2,6 @@ if (require(devtools) & require(roxygen2) & require(Rcpp) & require(drat)) {
   rmarkdown::render(input = "docs/index.Rmd", output_file = "index.html") 
   rmarkdown::render(input = "README.Rmd", output_file = "README.md") 
 
-
   Rcpp::compileAttributes() 
   devtools::document()
   devtools::load_all()
